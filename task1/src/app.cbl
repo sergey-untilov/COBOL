4cd64024631eff4b07f000dd7540f5448a14f85c
sergey-untilov <s.untilov@gmail.com>
2018-07-30 08:49:58 +0300

    Minor updates

--- task1/src/app.cbl
+++ task1/src/app.cbl
@@ -3,7 +3,7 @@
       *
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-           01  args    PIC X(40).
+           01  args    PIC X(50).
       *
        PROCEDURE DIVISION.
            ACCEPT args FROM COMMAND-LINE.
