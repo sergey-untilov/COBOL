4f037fd709a5a86b87797b61e76b505606a94a7f
sergey-untilov <s.untilov@gmail.com>
2018-07-30 09:03:38 +0300

    Copy alphanumeric string to null-terminated string

--- task1/src/app.cbl
+++ task1/src/app.cbl
@@ -4,9 +4,13 @@
        DATA DIVISION.
        WORKING-STORAGE SECTION.
            01  args    PIC X(50).
+           01  args0   PIC X(51).
       *
        PROCEDURE DIVISION.
            ACCEPT args FROM COMMAND-LINE.
       *    ACCEPT args FROM STDIN
-           call "shared_print" using by reference args.
+           String args delimited by size
+            X'00' delimited by size
+            into args0.
+           call "shared_print" using by reference args0.
            stop run.
